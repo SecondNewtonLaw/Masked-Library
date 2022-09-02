68b638a91060aeb11bd692c9a5e706e271ea650b
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-09-02 20:37:40 -0300

    Remove no await in async method warning.

--- src/Additions/System/Extensions/String.cs
+++ src/Additions/System/Extensions/String.cs
@@ -41,11 +41,14 @@ public static class StringExt
     /// <returns>A True if the string contains any of the characters in the collection</returns>
     public static async Task<bool> Contains(this string str, ICollection<char> chars)
     {
-        for (int i = 0; i < str.Length; i++)
+        return await new Task<bool>(() =>
         {
-            if (chars.Contains(str[i]))
-                return true;
-        }
-        return false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (chars.Contains(str[i]))
+                    return true;
+            }
+            return false;
+        });
     }
 }
