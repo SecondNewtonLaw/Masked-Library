a87fc2e8f00add999eaa0dda83f232e307d4275b
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-09-23 21:30:56 -0300

    Rename file, remove async modifier when not required

--- src/Additions/Discord/Extensions/UserExt.cs
+++ src/Additions/Discord/Extensions/UserExt.cs
@@ -5,8 +5,9 @@ namespace Masked.DiscordNet.Extensions;
 
 public static class UserExt
 {
-    public static async Task<bool> HasRole(this SocketGuildUser user, SocketRole role)
-        => await Task.Run(() => user.Roles.Contains(role));
+    public static Task<bool> HasRole(this SocketGuildUser user, SocketRole role)
+        => Task.Run(() => user.Roles.Contains(role));
+
     public static SocketGuildUser GetGuildUser<T>(this T userSocket) where T : IUser
         => (userSocket as SocketGuildUser)!;
 }
