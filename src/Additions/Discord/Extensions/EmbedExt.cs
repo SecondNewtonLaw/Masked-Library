102e385920da99b002bff601925dce571002cf50
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-11-22 00:24:36 -0300

    Create SetRandomColor(EmbedBuilder)

--- src/Additions/Discord/Extensions/EmbedExt.cs
+++ src/Additions/Discord/Extensions/EmbedExt.cs
@@ -1,5 +1,19 @@
+using Discord;
+
 namespace Masked.DiscordNet.Extensions;
 
 public static class EmbedExt
 {
+    /// <summary>
+    /// Creates a Random color and sets it on the EmbedBuilder.
+    /// </summary>
+    /// <param name="embedBuilder">Instance of EmbedBuilder</param>
+    public static void SetRandomColor(this EmbedBuilder embedBuilder)
+    {
+        int r = Random.Shared.Next(255);
+        int g = Random.Shared.Next(255);
+        int b = Random.Shared.Next(255);
+
+        embedBuilder.Color = new Color(r, g, b);
+    }
 }
