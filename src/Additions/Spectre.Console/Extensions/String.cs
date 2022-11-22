f45179deaddac29ff810dfbb4c9227926ef83993
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-11-22 00:23:32 -0300

    Fix Typo, and Documentation

--- src/Additions/Spectre.Console/Extensions/String.cs
+++ src/Additions/Spectre.Console/Extensions/String.cs
@@ -5,7 +5,7 @@ public static class StringExtSConsole
     /// <summary>
     /// Fixes markup syntax errors
     /// </summary>
-    /// <param name="markupeddString">The string with raw markup</param>
-    /// /// <returns>a string with it's markup fixed</returns>
+    /// <param name="markupedString">The string with raw markup</param>
+    /// <returns>a string with it's markup fixed</returns>
     public static string FixMarkup(this string markupedString) => markupedString.Replace("[", "[[").Replace("]", "]]");
 }
